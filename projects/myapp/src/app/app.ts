b5b446595ebdb0119cdfb5196718a12fe4adf999
Nathan Alberg <alberg.nate@gmail.com>
2025-06-05 11:37:21 -0500

    @import scss files don't do HMR with tailwind

---
+++ projects/myapp/src/app/app.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.html',
+  styleUrls: ['./app.scss'],
+  // styles:`
+  //   .componentStyles{
+  //     background-color: green;
+  //   }
+  // `
+})
+export class App {}
